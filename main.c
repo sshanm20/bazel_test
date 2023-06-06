dd4d4d47988ea45842c273d1d8b0ed12571626da
sshanm20 <134934386+sshanm20@users.noreply.github.com>
2023-06-06 17:07:30 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,7 @@
-#include <ctime>
-#include <string>
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-std::string get_greet(const std::string& who) {
-  return "Hello " + who;
-}
-
-void print_localtime() {
-  std::time_t result = std::time(nullptr);
-  std::cout << std::asctime(std::localtime(&result));
-}
-
-int main(int argc, char** argv) {
-  std::string who = "world";
-  if (argc > 1) {
-    who = argv[1];
-  }
-  std::cout << get_greet(who) << std::endl;
-  print_localtime();
-  return 0;
+main()
+{
+    cout<<"Hello World !";
 }
